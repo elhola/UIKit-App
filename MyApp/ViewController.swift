f4e268c3e86968e63e761a826344c0afc4516cca
Yaroslav G <yarmcfly@gmail.com>
2023-06-07 11:42:32 +0300

    Initial Commit

---
+++ MyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyApp
+//
+//  Created by Yaroslav G on 07.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
